3d5792744a7e6b1e3cecf1a49fbd56c47c6cfe8d
takezoux2 <takeshita@geishatokyo.com>
2013-08-22 15:24:04 +0900

    Add export selected objects as png script..

---
+++ README.md
@@ -0,0 +1,31 @@
+#
+
+## 導入方法
+
+Mac
+    /Applications/Adobe Illustrator CS6/Presets.localized/ja_JP/スクリプト
+
+(Illustratorのバージョンによってパスはかわることがあるので、パスは自分の環境で確認して下さい)
+
+に移動し、このレポジトリを
+    git clone git@github.com:geishatokyo/illustrator-scripts.git GTE-Scripts
+のコマンドでチェックアウトしてください。
+GTE-Scriptsの部分はディレクトリ名なので、好きな名前に変更して大丈夫です。
+チェックアウトが完了したら、Illustratorを再起動してください。
+
+## 実行方法
+
+導入できたら、上部のメニュ>ファイル>スクリプトに、各種スクリプトが追加されています。
+
+
+
+# スクリプトの種類
+
+## 選択したオブジェクトをPNGで保存
+
+選択しているオブジェクトだけをPNGとして保存します。
+
+
+
+
+
---
+++ 選択オブジェクトをPNGで保存.js
@@ -0,0 +1,56 @@
+
+
+var doc = app.activeDocument;
+
+var sels = doc.selection;
+
+function saveAsPNG( doc , filename){
+	var exportOptions = new ExportOptionsPNG24();
+	var type = ExportType.PNG24;
+	var fileSpec = new File(filename);
+	exportOptions.antiAliasing = false;
+	exportOptions.transparency = false;
+	exportOptions.saveAsHTML = true;
+
+	doc.exportFile(fileSpec,type,exportOptions)
+
+}
+
+function createNewDocument(items ){
+	var newDoc = app.documents.add();
+	for(var i = 0; i < items.length;i++){
+		var item = items[i];
+		if(item.duplicate){
+		    var copy = item.duplicate();
+		    copy.moveToEnd(newDoc);
+	    }
+	}
+	return newDoc;
+
+}
+
+
+function getDefaultName(items){
+	for(var i = 0;i < items.length ; i++){
+		var n = items[i].name;
+		if(n != null && n.length > 0){
+			return n;
+		}
+	}
+	return null;
+}
+
+if(sels.length == 0){
+	alert("オブジェクトを選択してください。");
+}else{
+	var filename = File.saveDialog("保存ファイル","*.png");
+	if(filename){
+		alert(filename);
+		var doc = createNewDocument(sels);
+		saveAsPNG(doc,filename);
+		doc.close(SaveOptions.DONOTSAVECHANGES);
+	}
+}
+
+
+
